647cf0bbb0a4be7186eb62e212e73b19e9fb83b7
Akash Kothawale <ak4pict@gmail.com>
2013-05-25 16:57:00 +0530

    Copies short url to clipboard
    Depends on xclip

--- lmgtfy/run.py
+++ lmgtfy/run.py
@@ -1,13 +1,19 @@
 from lmgtfy import Lmgtfy
+import requests
 import sys
+import os
 
 
 def google_it(query):
-    googler = Lmgtfy()
-    print "Lmgtfy url:", googler.lmgtfy_url(query)
+    lmgtfyer = Lmgtfy()
+    print "Lmgtfy url:", lmgtfyer.lmgtfy_url(query)
     try:
-        print "Short url :", googler.short_url(query)
-    except:
+        short_url = lmgtfyer.short_url(query)
+        print "Short url :", short_url
+        os.system("echo '" + short_url + "' | xclip -sel clip")
+        print "Short url copied to clipboard"
+
+    except requests.exceptions.ConnectionError:
         print "No Internet connection"
 
 
