780d1e5e1e7ee4651daccbca29377454b99a9884
Akash Kothawale <ak4pict@gmail.com>
2013-05-25 17:20:26 +0530

    Update requests version, and lmgtfy version

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('README.md') as file:
 setup(
     name='lmgtfy',
     description='Let me Google that for you',
-    version='0.1',
+    version='0.1.1',
     packages=['lmgtfy'],
     long_description=long_description,
     license='MIT',
     author='Akash Kothawale',
     platforms='Linux',
     author_email='io@decached.com',
-    install_requires='requests==1.2.0',
+    install_requires='requests>=1.2.0',
     entry_points={'console_scripts': ['lmgtfy = lmgtfy.run:command_line_runner']},
 )
